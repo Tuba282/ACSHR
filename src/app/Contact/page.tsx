bd8c9c624ddc2113130ee2184b9afe8ed7e7153d
tubaj@TubaJan <tubajan282@gmail.com>
2024-05-18 17:03:02 +0500

    acshr

---
+++ src/app/Contact/page.tsx
@@ -0,0 +1,292 @@
+import Footer from "../Components/footer";
+import Navbar from "../Components/navbar";
+
+const Contact = () => {
+  return (
+    <div className="text-black-600 body-font">
+      {/* <Navbar/> */}
+      <Navbar />
+      <div className="contact_hero ">
+      <center>
+          <h1 className="text-8xl pt-64 text-white font-bold font-mono">
+            Contact us
+          </h1>
+        </center>
+      </div>
+      <div className="px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-20">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+            <b className="text-5xl text-lime-600">Our Teams Contacts </b>
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-2xl text-dark">
+            One of our team will be in contact with you shortly... We value your
+            enquiry and we will make every effort to contact you as soon as
+            possible. Member Evolution is a subsidiary of Aspedia Australia so
+            to respond with maximum efficiency you may be contacted by staff
+            form either company to discuss your requirements.
+          </p>
+        </div>
+      </div>
+
+      <br />
+      <br />
+      
+
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-20">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+            <b className="text-5xl text-lime-600">Find Us </b>
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-4xl">
+            Looking for our offices or headquarters? Nordea can be found in many
+            places of the world. On this page you`ll find addresses to
+            headquarters and places where we have our offices.
+          </p>
+        </div>
+      </div>
+
+      <div className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-20">
+            <b className="text-5xl text-lime-600 mb-1">
+              Head Office - Karachi
+            </b>
+            <br />
+            <h1 className="sm:text-2xl  font-medium title-font text-gray-900">
+              <br />
+              <b>ADDRESS:</b> A-104/1, SACHAL GOTH RESIDENCY, PCSIR SCHEME 33,
+              DISTRICT EASTKARACHI. <br />
+              <b>PHONE: </b> 0333-7335483, 0300-2575133, 0332-8786028,
+              0334-3667824 <br />
+              <b>EMAIL: </b> <u>acshr2021@gmail.com</u> <br />
+              <b>Website: </b> <u>https://acshr.vercel.app</u> <br />
+              <b> FACEBOOK: </b> ALLIANCE OF THE CIVIL SOCIETY FOR HUMAN RIGHTS{" "}
+              <br />
+            </h1>
+          </div>
+          <br />
+          <br />
+          <br />
+          <br />
+          <div className="flex flex-wrap -m-4">
+            <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-lime-600 text-white flex-shrink-0">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                      <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                  </div>
+                  <h2 className="text-black-900 text-lg title-font font-medium">
+                    <b>
+                      <u>DISTRICT OFFICE - DADU</u>
+                    </b>
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <p className="leading-relaxed text-base">
+                    <b>FOCAL PERSON:</b> MUHAMMAD ASHRAF <br />
+                    <b> ADDRESS:</b> HOUSE NO. 102, SOOMRA MUHALLAH, <br />{" "}
+                    PHULJI STATION, DISTRICT DADU. <br />
+                    <b> PHONE:</b> 0333-7335483 <br />
+                  </p>
+                 
+                </div>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-lime-600 text-white flex-shrink-0">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                      <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                  </div>
+                  <h2 className="text-gray-900 text-lg title-font font-medium">
+                    <b>
+                      <u>DISTRICT OFFICE – BADIN</u>
+                    </b>
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <p className="leading-relaxed text-base">
+                    <b>FOCAL PERSON:</b> RIZWAN ALI <br />
+                    <b> ADDRESS:</b> HOUSE NO. 08, ALI TOWN BYPASS ROAD, BADIN{" "}
+                    <br />
+                    <b> PHONE:</b> 0333-7335483 <br />
+                  </p>
+                 
+                </div>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-lime-600 text-white flex-shrink-0">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                      <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                  </div>
+                  <h2 className="text-gray-900 text-lg title-font font-medium">
+                    <b>
+                      <u>DISTRICT OFFICE - SUKKUR</u>
+                    </b>
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <p className="leading-relaxed text-base">
+                    <b> FOCAL PERSON: </b> SYED UMAIS SHAH <br />
+                    <b> ADDRESS: </b> HOUSE NO. D-516/133, MOLVI <br />
+                    ABDULLAH STREET NEAR NISHAT CINEMA, BARRAGE ROAD, SUKKUR.
+                    <br />
+                    <b>PHONE: </b> 0334-3667824 <br />
+                  </p>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="text-gray-600 body-font">
+        <div className="flex flex-wrap -m-4">
+          <div className="p-4 md:w-1/3">
+            <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+              <div className="flex items-center mb-3">
+                <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-lime-600 text-white flex-shrink-0">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                    <circle cx="12" cy="7" r="4"></circle>
+                  </svg>
+                </div>
+                <h2 className="text-gray-900 text-lg title-font font-medium">
+                  <b>
+                    <u>PROVINCIAL OFFICE - QUETTA</u>
+                  </b>
+                </h2>
+              </div>
+              <div className="flex-grow">
+                <p className="leading-relaxed text-base">
+                  <b>FOCAL PERSON: </b> ABDUL GHAFAR JAMALI <br />
+                  <b>ADDRESS: </b> HOUSE NO. 51-OPPOSITE FATIMA COMPLEX NEAR
+                  SAIGON RESTAURANT PHASE-3 SHAHBAZ TOWN, QUETTA <br />
+                  <b>PHONE: </b> 0331-8051921 <br />
+                </p>
+              
+              </div>
+            </div>
+          </div>
+          <div className="p-4 md:w-1/3">
+            <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+              <div className="flex items-center mb-3">
+                <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-lime-600 text-white flex-shrink-0">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                    >
+                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                    <circle cx="12" cy="7" r="4"></circle>
+                  </svg>
+                </div>
+                <h2 className="text-gray-900 text-lg title-font font-medium">
+                  <u>
+                    <b>DISTRICT OFFICE – KHAIRPUR MIRS</b>
+                  </u>
+                </h2>
+              </div>
+              <div className="flex-grow">
+                <p className="leading-relaxed text-base">
+                  <b> FOCAL PERSON:</b> ADEEL AHMED VISTRO <br />
+                  <b> ADDRESS:</b> HOUSE NO. F-416/133, SHEESHAM. <br />
+                  COLONY,LUQMAN PHATAK, KHAIRPUR MIRS,SINDH.
+                  <b> PHONE:</b> 0331-3756862 <br />
+                </p>
+             
+              </div>
+            </div>
+          </div>
+
+          <div className="p-4 md:w-1/3">
+            <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+              <div className="flex items-center mb-3">
+                <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-lime-600 text-white flex-shrink-0">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                    >
+                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                    <circle cx="12" cy="7" r="4"></circle>
+                  </svg>
+                </div>
+                <h2 className="text-gray-900 text-lg title-font font-medium">
+                  <u>
+                    <b>DISTRICT OFFICE - UMERKOT</b>
+                  </u>
+                </h2>
+              </div>
+              <div className="flex-grow">
+                <p className="leading-relaxed text-base">
+                  <b>FOCAL PERSON:</b> ABDUL HAQ SOOMRO <br />
+                  <b> ADDRESS:</b> HOUSE NO. E-101/122, NEAR GOVT HOSPITAL,
+                  UMEKOT,SINDH. <br />
+                  <b>PHONE:</b> 0333-3246809 <br />
+                </p>
+         
+              </div>
+            </div>
+          </div>
+        </div>
+                                  </div>
+        </div>
+
+     
+      </div> <br /><br /><br /><br />
+      <Footer/>
+    </div>
+  );
+};
+
+export default Contact;
